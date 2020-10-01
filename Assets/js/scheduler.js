17d2ed718cd7f35f5ada3ae85aaf0a12b636a91b
Meena Ambalam <meena.a.ambalam@gmail.com>
2020-10-01 12:44:36 -0500

    completed the main functionality

--- Assets/js/scheduler.js
+++ Assets/js/scheduler.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
     var CurrMin = CurrTime[1];
 
     var CurrDate = CurrWeekDay + " " + CurrMonth + " " + CurrDay + " " + CurrYear;
+    var dlyScheduleHr;
+    var dlyScheduleDesc;
 
     var dailyCalData;
     /* format in which the local storage data of the daily schedule will be kept 
@@ -29,52 +31,39 @@ $(document).ready(function() {
     */
 
     //Display the Current Date in the Jumbotron
+ 
     $("#currentDay").text(CurrDate);
+    console.log(CurrHr);
     
-    var dlySchedule = JSON.parse(localStorage.getItem("dlySchedule"));
     //Go through the textarea and add specific class to reflect the CSS settings
     $(".hour").each(function(){
-
         var hourValue = parseInt($(this).attr("value"));
-        if (hourValue < CurrTime) {
+
+        dlyScheduleHr = "dlySchedule" + hourValue;
+        dlyScheduleDesc = localStorage.getItem(dlyScheduleHr);
+
+        if (hourValue < CurrHr) {
             $(this).addClass("past");
-        } else if(hourValue > CurrTime){
+        } else if(hourValue > CurrHr){
             $(this).addClass("future");
         } else {
             $(this).addClass("present");
         }
-        /*
-        if (parseInt(dlySchedule.hrBlock) == hourValue){
-            $(this).val(dlySchedule.desc);
-        }*/
+        $(this).val(dlyScheduleDesc);
     })
 
     //Save Btn click - save the description into localstorage for future 
     $(".saveBtn").on("click",function(){
         var btnValue = parseInt($(this).attr("value"));
-        var dlySchedule = [
-            {hrBlock: 9, desc: ""},
-            {hrBlock: 10, desc: ""},
-            {hrBlock: 11, desc: ""},    
-            {hrBlock: 12, desc: ""},
-            {hrBlock: 13, desc: ""},
-            {hrBlock: 14, desc: ""},
-            {hrBlock: 15, desc: ""},
-            {hrBlock: 16, desc: ""},
-            {hrBlock: 17, desc: ""}
-        ]
-                
+        alert("saving");
 
         $(".hour").each(function(){
             var hourValue = parseInt($(this).attr("value"));
             if (hourValue == btnValue) {
-                alert("saving info to local storage");
-                dlySchedule.hrBlock = btnValue;
-                alert("this desc value " + $(this).val());
-                dlySchedule.desc = $(this).val();
-                var saveArrIndex = btnValue - 9;
-                console.log(saveArrIndex);
-                localStorage.setItem("dlySchedule", JSON.stringify(dlySchedule[saveArrIndex]));
+                dlyScheduleDesc = $(this).val();
+                dlyScheduleHr = "dlySchedule" + btnValue;
+                console.log("variable dynamically created: " + dlyScheduleHr);
+                localStorage.setItem(dlyScheduleHr, dlyScheduleDesc);
             }
         })
 
